03beab8a33b4da38687bde550a525cc6049d7814
ShanteTorres <kennedy2110@gmail.com>
2023-04-28 23:03:17 -0400

    Add contact

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const myList = document.getElementById('myList');
-const listItems = myList.getElementsByTagName('li');
-for (let i = 0; i < listItems.length; i++) {
-  listItems[i].addEventListener('click', function() {
-    // Message
-  });
-}
+// const myList = document.getElementById('myList');
+// const listItems = myList.getElementsByTagName('li');
+// for (let i = 0; i < listItems.length; i++) {
+//   listItems[i].addEventListener('click', function() {
+//     // Message
+//   });
+// }
+document.getElementById('Contact').addEventListener('click', function() {
+  // Scroll to the desired section on the page
+  document.getElementById('targetSection').scrollIntoView({ behavior: 'smooth' });
+});
